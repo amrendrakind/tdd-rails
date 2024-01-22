67a0a7941d771a81430f0b7e0eeb466242e4bbb5
amrendrakind <vision2010@gmail.com>
2024-01-22 12:38:11 +0530

    test is passing for blank title and content

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is has valid attributes' do
+    post = FactoryBot.create(:post)
+    expect(post).to be_valid
+  end
+  # pending "add some examples to (or delete) #{__FILE__}"
 end
