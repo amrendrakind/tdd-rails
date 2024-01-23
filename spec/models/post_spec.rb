e064c1e34d422ada5acaa901412e79af322448dd
amrendrakind <vision2010@gmail.com>
2024-01-23 20:41:23 +0530

    Post unit test update

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it 'is has valid attributes' do
-    post = FactoryBot.create(:post)
-    expect(post).to be_valid
+  describe 'Post' do
+    it 'is has valid attributes' do
+      post = FactoryBot.create(:post)
+      expect(post).to be_valid
+    end
+
+    it 'is has valid attributes' do
+      post = FactoryBot.create(:post)
+      expect(post).to be_valid
+    end
   end
-  # pending "add some examples to (or delete) #{__FILE__}"
+    # pending "add some examples to (or delete) #{__FILE__}"
 end
